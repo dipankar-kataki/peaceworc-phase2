84cf7ccd6c2942464239f7631a71d13881d5407a
dipankar kataki <dipankar.kataki@ekodus.com>
2022-11-10 17:43:30 +0530

    login screen

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,4 +1,4 @@
-<!doctype html>
+<!Doctype html>
 <html lang="en" data-layout="vertical" data-topbar="light" data-sidebar="dark" data-sidebar-size="lg" data-sidebar-image="none" data-preloader="disable">
 
   <head>
