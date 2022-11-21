4689b949090f329de0af51b1fcc502cdc9209c8f
dipankar kataki <dipankar.kataki@ekodus.com>
2022-11-21 10:49:27 +0530

    status

--- app/Http/Controllers/Agency/Status/InformationStatusController.php
+++ app/Http/Controllers/Agency/Status/InformationStatusController.php
@@ -14,6 +14,13 @@ class InformationStatusController extends Controller
 
     public function informationStatus(){
         $status = AgencyInformationStatus::where('user_id', Auth::user()->id)->first();
+        if($status == null){
+            $status = [
+                'is_registration_complete' => 0,
+                'is_authorize_info_added' => 0,
+                'is_profile_approved' => 0
+            ];
+        }
         return $this->success('Great! Agency Information Status Fetched Successfully', $status, null , 200);
     }
 }
