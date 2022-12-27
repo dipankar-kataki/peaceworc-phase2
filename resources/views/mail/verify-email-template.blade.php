04974efaae16bb959d541401c1e4e681ee8ae0a1
dipankar kataki <dipankar.kataki@ekodus.com>
2022-12-27 14:29:55 +0530

    mailing

--- resources/views/mail/verify-email-template.blade.php
+++ resources/views/mail/verify-email-template.blade.php
@@ -60,71 +60,10 @@
             <h4 style="font-weight: 500; line-height: 1.5;font-family: 'Roboto', sans-serif;">Need Help ?</h4>
             <p style="color: #878a99; line-height: 1.5;">Please send and feedback or bug info to <a href="#" style="font-weight: 500;">info@peaceworc.com</a></p>
             <p style="font-family: 'Roboto', sans-serif; font-size: 14px;color: #98a6ad; margin: 0px;">
-                {{date('Y')}} &copy www.peaceworc.com. All rights reserved.
+                {{date('Y')}} &copy; www.peaceworc.com. All rights reserved.
             </p>
         </div>
     </div>
-    <!--100% body table-->
-    {{-- <table cellspacing="0" border="0" cellpadding="0" width="100%" bgcolor="#f2f3f8"
-        style="@import url(https://fonts.googleapis.com/css?family=Rubik:300,400,500,700|Open+Sans:300,400,600,700); font-family: 'Open Sans', sans-serif;">
-        <tr>
-            <td>
-                <table style="background-color: #f2f3f8; max-width:670px;  margin:0 auto;" width="100%" border="0"
-                    align="center" cellpadding="0" cellspacing="0">
-                    <tr>
-                        <td style="height:80px;">&nbsp;</td>
-                    </tr>
-                    <tr>
-                        <td style="text-align:center;">
-                          <a href="#" title="logo" target="_blank">
-                            <img width="200" src="/assets/images/logo/logo.png" title="logo" alt="logo">
-                          </a>
-                        </td>
-                    </tr>
-                    <tr>
-                        <td style="height:20px;">&nbsp;</td>
-                    </tr>
-                    <tr>
-                        <td>
-                            <table width="95%" border="0" align="center" cellpadding="0" cellspacing="0"
-                                style="max-width:670px;background:#fff; border-radius:3px; text-align:center;-webkit-box-shadow:0 6px 18px 0 rgba(0,0,0,.06);-moz-box-shadow:0 6px 18px 0 rgba(0,0,0,.06);box-shadow:0 6px 18px 0 rgba(0,0,0,.06);">
-                                <tr>
-                                    <td style="height:40px;">&nbsp;</td>
-                                </tr>
-                                <tr>
-                                    <td style="padding:0 35px;">
-                                        <h4>Hello, Greetings from Peaceworc.
-                                        </h4>
-                                        <h3 style="color:#1e1e2d; font-weight:500; margin:0;font-size:32px;font-family:'Rubik',sans-serif;">Your Email Verification OTP is: {{$otp}}</h3>
-                                    </td>
-                                </tr>
-                                <tr>
-                                    <td style="height:40px;">&nbsp;</td>
-                                </tr>
-                            </table>
-                        </td>
-                    <tr>
-                        <td style="height:20px;">&nbsp;</td>
-                    </tr>
-                    <tr>
-                        <td style="text-align:center;">
-                            <p style="font-size:14px; color:rgba(69, 80, 86, 0.7411764705882353); line-height:18px; margin:0 0 0;">&copy; 
-                                <strong>
-                                    @if (Illuminate\Support\Str::startsWith(Request::root(), 'http://'))
-                                        www.{{substr(Request::root(), 7) }}
-                                    @endif
-                                </strong>
-                            </p>
-                        </td>
-                    </tr>
-                    <tr>
-                        <td style="height:80px;">&nbsp;</td>
-                    </tr>
-                </table>
-            </td>
-        </tr>
-    </table> --}}
-    <!--/100% body table-->
 </body>
 
 </html>
