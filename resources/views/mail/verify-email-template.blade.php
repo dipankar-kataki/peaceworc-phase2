d38274d390d4d1d362e8dec953109438931140f7
dipankar kataki <dipankar.kataki@ekodus.com>
2022-12-27 12:18:37 +0530

    mailing

--- resources/views/mail/verify-email-template.blade.php
+++ resources/views/mail/verify-email-template.blade.php
@@ -4,7 +4,7 @@
 
 <head>
     <meta content="text/html; charset=utf-8" http-equiv="Content-Type" />
-    <title>Email Verification Mail</title>
+    <title>Peaceworc | Email Verification Mail</title>
     <meta name="description" content="Email Verification Mail Template.">
     <style type="text/css">
         a:hover {text-decoration: underline !important;}
@@ -12,8 +12,51 @@
 </head>
 
 <body marginheight="0" topmargin="0" marginwidth="0" style="margin: 0px; background-color: #f2f3f8;" leftmargin="0">
+
+    <div class="content" style="font-family: 'Roboto', sans-serif; box-sizing: border-box; font-size: 14px; max-width: 600px; display: block; margin: 0 auto; padding: 20px;">
+        <table class="main" width="100%" cellpadding="0" cellspacing="0" style="font-family: 'Roboto', sans-serif; box-sizing: border-box; font-size: 14px; border-radius: 3px; margin: 0; border: none;">
+            <tbody>
+                <tr style="font-family: 'Roboto', sans-serif; font-size: 14px; margin: 0;">
+                    <td class="content-wrap" style="font-family: 'Roboto', sans-serif; box-sizing: border-box; color: #495057; font-size: 14px; vertical-align: top; margin: 0;padding: 30px; box-shadow: 0 3px 15px rgba(30,32,37,.06); ;border-radius: 7px; background-color: #fff;" valign="top">
+                        <meta itemprop="name" content="Confirm Email" style="font-family: 'Roboto', sans-serif; box-sizing: border-box; font-size: 14px; margin: 0;">
+                        <table width="100%" cellpadding="0" cellspacing="0" style="font-family: 'Roboto', sans-serif; box-sizing: border-box; font-size: 14px; margin: 0;">
+                            <tbody><tr style="font-family: 'Roboto', sans-serif; box-sizing: border-box; font-size: 14px; margin: 0;">
+                                <td class="content-block" style="font-family: 'Roboto', sans-serif; box-sizing: border-box; font-size: 14px; vertical-align: top; margin: 0; padding: 0 0 20px;" valign="top">
+                                    <div style="text-align: center;margin-bottom: 15px;">
+                                        <img src="/assets/images/logo/logo.png" alt="" height="23">
+                                    </div>
+                                </td>
+                            </tr>
+                            <tr style="font-family: 'Roboto', sans-serif; box-sizing: border-box; font-size: 14px; margin: 0;">
+                                <td class="content-block" style="font-family: 'Roboto', sans-serif; box-sizing: border-box; font-size: 24px; vertical-align: top; margin: 0; padding: 0 0 10px;  text-align: center;" valign="top">
+                                    <h4 style="font-family: 'Roboto', sans-serif; font-weight: 500;">Your One Time Password For Email Verification</h4>
+                                </td>
+                            </tr>
+                            <tr style="font-family: 'Roboto', sans-serif; box-sizing: border-box; font-size: 14px; margin: 0;">
+                                <td class="content-block" style="font-family: 'Roboto', sans-serif; box-sizing: border-box; font-size: 14px; vertical-align: top; margin: 0; padding: 0 0 22px; text-align: center;">
+                                    <a href="#" style="font-family: 'Roboto', sans-serif; box-sizing: border-box; font-size: 1rem; color: #FFF; text-decoration: none; font-weight: 400; text-align: center; cursor: pointer; display: inline-block; border-radius: .25rem; text-transform: capitalize; background-color: #405189; margin: 0; border-color: #405189; border-style: solid; border-width: 1px; padding: .5rem .9rem;">
+                                        {{$otp}}
+                                    </a>
+                                </td>
+                            </tr>
+                        </tbody></table>
+                    </td>
+                </tr>
+            </tbody>
+        </table>
+        <div style="text-align: center; margin: 25px auto 0px auto;font-family: 'Roboto', sans-serif;">
+            <h4 style="font-weight: 500; line-height: 1.5;font-family: 'Roboto', sans-serif;">Need Help ?</h4>
+            <p style="color: #878a99; line-height: 1.5;">Please send and feedback or bug info to <a href="#" style="font-weight: 500;">info@peaceworc.com</a></p>
+            <p style="font-family: 'Roboto', sans-serif; font-size: 14px;color: #98a6ad; margin: 0px;">
+                {{date('Y')}} &copy 
+                @if (Illuminate\Support\Str::startsWith(Request::root(), 'http://'))
+                    www.{{substr(Request::root(), 7) }}
+                @endif
+            </p>
+        </div>
+    </div>
     <!--100% body table-->
-    <table cellspacing="0" border="0" cellpadding="0" width="100%" bgcolor="#f2f3f8"
+    {{-- <table cellspacing="0" border="0" cellpadding="0" width="100%" bgcolor="#f2f3f8"
         style="@import url(https://fonts.googleapis.com/css?family=Rubik:300,400,500,700|Open+Sans:300,400,600,700); font-family: 'Open Sans', sans-serif;">
         <tr>
             <td>
@@ -71,7 +114,7 @@
                 </table>
             </td>
         </tr>
-    </table>
+    </table> --}}
     <!--/100% body table-->
 </body>
 
