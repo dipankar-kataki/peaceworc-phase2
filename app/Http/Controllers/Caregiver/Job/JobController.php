cfbca506ae09f41bd32c6729d0ad950f179418f0
dipankar kataki <dipankar.kataki@ekodus.com>
2022-12-28 17:23:30 +0530

    date format agency job posting

--- app/Http/Controllers/Caregiver/Job/JobController.php
+++ app/Http/Controllers/Caregiver/Job/JobController.php
@@ -119,7 +119,39 @@ class JobController extends Controller
     }
 
     public function getQuickCallJobs(){
-        $get_all_jobs = AgencyPostJob::where('status', JobStatus::QuickCall)->get();
-        return $this->success('Great! Jobs fetched successfully.', $get_all_jobs, null, 200);
+        $get_jobs = AgencyPostJob::where('status', JobStatus::QuickCall)->get();
+
+        $get_job_details = [];
+
+        foreach($get_jobs as $job){
+            $job_owner = AgencyProfileRegistration::with('user')->where('user_id', $job->user_id)->first();
+            $details = [
+                'job_id' => $job->id,
+                'company_name' => $job_owner->company_name,
+                'company_photo' => $job_owner->photo,
+                'job_title' => $job->title,
+                'care_type' => $job->care_type,
+                'care_items' => $job->care_items,
+                'date' => $job->date,
+                'start_time' => $job->start_time,
+                'end_time' => $job->end_time,
+                'amount' => $job->amount,
+                'address' => $job->address,
+                'description' => $job->description,
+                'medical_history' => $job->medical_history,
+                'experties' => $job->experties,
+                'other_requirements' => $job->other_requirements,
+                'check_list' => $job->check_list,
+                'status' => $job->status,
+                // 'bidding_start_time' => $job->bidding_start_time,
+                // 'bidding_end_time' => $job->bidding_end_time,
+                'created_at' => $job->created_at->diffForHumans(),
+
+            ];
+
+            array_push($get_job_details, $details);
+        }
+
+        return $this->success('Great! Job Fetched Successfully', $get_job_details, null, 200);
     }
 }
