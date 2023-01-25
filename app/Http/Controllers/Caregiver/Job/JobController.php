76b66a34dac5a219786e08f93b6183d4ba85abd2
dipankar kataki <dipankar.kataki@ekodus.com>
2023-01-25 11:09:39 +0530

    get single job for bidding modified

--- app/Http/Controllers/Caregiver/Job/JobController.php
+++ app/Http/Controllers/Caregiver/Job/JobController.php
@@ -136,37 +136,41 @@ class JobController extends Controller
         if(!isset($_GET['job_id'])){
             return $this->error('Oops! Invalid Params Passed', null, null, 400);
         }else{
-            $get_job_for_bidding = AgencyPostJob::where('id', $_GET['job_id'])->first();
-
-            $job_owner = AgencyProfileRegistration::with('user')->where('user_id', $get_job_for_bidding->user_id)->first();
-            $details = [
-                'job_id' => $get_job_for_bidding->id,
-                'company_name' => $job_owner->company_name,
-                'company_photo' => $job_owner->photo,
-                'job_title' => $get_job_for_bidding->title,
-                'care_type' => $get_job_for_bidding->care_type,
-                'care_items' => $get_job_for_bidding->care_items,
-                'date' => $get_job_for_bidding->date,
-                'start_time' => $get_job_for_bidding->start_time,
-                'end_time' => $get_job_for_bidding->end_time,
-                'amount' => $get_job_for_bidding->amount,
-                'address' => $get_job_for_bidding->address,
-                'short_address' => $get_job_for_bidding->short_address,
-                'lat' => $get_job_for_bidding->lat,
-                'long' => $get_job_for_bidding->long,
-                'description' => $get_job_for_bidding->description,
-                'medical_history' => $get_job_for_bidding->medical_history,
-                'experties' => $get_job_for_bidding->experties,
-                'other_requirements' => $get_job_for_bidding->other_requirements,
-                'check_list' => $get_job_for_bidding->check_list,
-                'status' => $get_job_for_bidding->status,
-                'bidding_start_time' => $get_job_for_bidding->bidding_start_time,
-                'bidding_end_time' => $get_job_for_bidding->bidding_end_time,
-                'created_at' => $get_job_for_bidding->created_at->diffForHumans(),
-
-            ];
-
-            return $this->success('Great! Job Fetched Successfully', $details, null, 200);
+            $get_job_for_bidding = AgencyPostJob::where('id', $_GET['job_id'])->where('status', JobStatus::BiddingStarted)->first();
+            if($get_job_for_bidding == null){
+                return $this->error('Oops! Invalid Request', null, null, 500);
+            }else{
+                $job_owner = AgencyProfileRegistration::with('user')->where('user_id', $get_job_for_bidding->user_id)->first();
+                $details = [
+                    'job_id' => $get_job_for_bidding->id,
+                    'company_name' => $job_owner->company_name,
+                    'company_photo' => $job_owner->photo,
+                    'job_title' => $get_job_for_bidding->title,
+                    'care_type' => $get_job_for_bidding->care_type,
+                    'care_items' => $get_job_for_bidding->care_items,
+                    'date' => $get_job_for_bidding->date,
+                    'start_time' => $get_job_for_bidding->start_time,
+                    'end_time' => $get_job_for_bidding->end_time,
+                    'amount' => $get_job_for_bidding->amount,
+                    'address' => $get_job_for_bidding->address,
+                    'short_address' => $get_job_for_bidding->short_address,
+                    'lat' => $get_job_for_bidding->lat,
+                    'long' => $get_job_for_bidding->long,
+                    'description' => $get_job_for_bidding->description,
+                    'medical_history' => $get_job_for_bidding->medical_history,
+                    'experties' => $get_job_for_bidding->experties,
+                    'other_requirements' => $get_job_for_bidding->other_requirements,
+                    'check_list' => $get_job_for_bidding->check_list,
+                    'status' => $get_job_for_bidding->status,
+                    'bidding_start_time' => $get_job_for_bidding->bidding_start_time,
+                    'bidding_end_time' => $get_job_for_bidding->bidding_end_time,
+                    'created_at' => $get_job_for_bidding->created_at->diffForHumans(),
+    
+                ];
+    
+                return $this->success('Great! Job Fetched Successfully', $details, null, 200);
+            }
+            
         }
     }
 
