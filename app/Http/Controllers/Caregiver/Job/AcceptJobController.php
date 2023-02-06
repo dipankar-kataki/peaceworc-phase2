7aca687cc6875a30817bf8f2ceaf16409d2cceab
dipankar kataki <dipankar.kataki@ekodus.com>
2023-02-06 15:51:48 +0530

    ongoing

--- app/Http/Controllers/Caregiver/Job/AcceptJobController.php
+++ app/Http/Controllers/Caregiver/Job/AcceptJobController.php
@@ -13,30 +13,24 @@ use Illuminate\Support\Facades\Auth;
 class AcceptJobController extends Controller
 {
     use ApiResponse;
-    public function acceptJob(){
-        if(!isset($_GET['job_id'])){
-            return $this->error('Oops! Something Went Wrong. Failed To Accept job.', null, null, 500);
-        }else{
-            if($_GET['job_id'] == null){
-                return $this->error('Oops! Something Went Wrong. Failed To Accept job.', null, null, 500);
-            }else{
-                try{
-                    $create = AcceptJob::create([
-                        'user_id' => Auth::user()->id,
-                        'job_id' => $_GET['job_id'],
-                        'status' => JobStatus::JobAccepted
-                    ]);
+    public function acceptJob(Request $request){
+        
+        try{
+            $create = AcceptJob::create([
+                'user_id' => Auth::user()->id,
+                'job_id' => $request->job_id,
+                'status' => JobStatus::JobAccepted
+            ]);
 
-                    if($create){
-                        AgencyPostJob::where('id', $_GET['job_id'])->update([
-                            'status' => JobStatus::JobAccepted
-                        ]);
-                    }
-                    return $this->success('Great! Job Accepted Successfully', null, null, 201);
-                }catch(\Exception $e){
-                    return $this->error('Oops! Something Went Wrong. Failed To Accept job.', null, null, 500);
-                }
+            if($create){
+                AgencyPostJob::where('id', $request->job_id)->update([
+                    'status' => JobStatus::JobAccepted
+                ]);
             }
+            return $this->success('Great! Job Accepted Successfully', null, null, 201);
+        }catch(\Exception $e){
+            return $this->error('Oops! Something Went Wrong. Failed To Accept job.', null, null, 500);
         }
+           
     }
 }
