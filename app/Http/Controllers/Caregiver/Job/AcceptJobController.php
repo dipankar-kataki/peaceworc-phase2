25c3b0862a7193d17b89dab01665eb0310c2d6ef
dipankar kataki <dipankar.kataki@ekodus.com>
2023-03-01 10:34:54 +0530

    delete api caregiver document

--- app/Http/Controllers/Caregiver/Job/AcceptJobController.php
+++ app/Http/Controllers/Caregiver/Job/AcceptJobController.php
@@ -20,7 +20,7 @@ class AcceptJobController extends Controller
             $check_if_status_exists = CaregiverStatusInformation::where('user_id',Auth::user()->id )->first();
             if($check_if_status_exists != null){
                 if($check_if_status_exists){
-                    if($check_if_status_exists->is_basic_info_added == 0){
+                    if($check_if_status_exists->is_basic_info_added == 0 || $check_if_status_exists->is_documents_uploaded == 0){
                         return $this->error('Oops! Profile Incomplete. Failed To Accept job.', null, null, 500);
                     }else{
                         $create = AcceptJob::create([
