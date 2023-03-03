bdc36f940db76e69abb92dad825044962b4a4266
dipankar kataki <dipankar.kataki@ekodus.com>
2023-03-03 16:32:30 +0530

    update basic info

--- app/Http/Controllers/Caregiver/Registration/ProfileRegistrationController.php
+++ app/Http/Controllers/Caregiver/Registration/ProfileRegistrationController.php
@@ -31,21 +31,17 @@ class ProfileRegistrationController extends Controller
             return $this->error('Oops! Validation Failed. '.$validator->errors()->first(), null, null, 400);
         }else{
             try{
-                $check_if_phone_exists = CaregiverProfileRegistration::where('phone', $request->phone)->exists();
-                $check_if_ssn_exists = CaregiverProfileRegistration::where('ssn', $request->ssn)->exists();
-                if($check_if_phone_exists){
-                    return $this->error('Oops! Failed To Register. Phone Number Associated With Another User.', null, null, 400);
-                }else if($check_if_ssn_exists){
-                    return $this->error('Oops! Failed To Register. SSN Number Associated With Another User.', null, null, 400);
-                }else{
+
+                $check_if_user_exists = CaregiverProfileRegistration::where('user_id', Auth::user()->id)->exists();
+                if($check_if_user_exists){
 
                     if ($request->hasFile('photo')) {
                         $image = time() . '.' . $request->photo->extension();
                         $request->photo->move(public_path('Caregiver/Uploads/Profile/image/'), $image);
                         $imageName = 'Caregiver/Uploads/Profile/image/' . $image;
                     }
-                   
-                    $create = CaregiverProfileRegistration::create([
+
+                    $update = CaregiverProfileRegistration::where('user_id', Auth::user()->id)->update([
                         'user_id' => Auth::user()->id,
                         'photo' => $imageName,
                         'phone' => $request->phone,
@@ -56,21 +52,54 @@ class ProfileRegistrationController extends Controller
                         'short_address' => $request->short_address
                     ]);
 
-                    if($create){
-                        try{
-                            CaregiverStatusInformation::create([
-                                'user_id' => Auth::user()->id,
-                                'is_basic_info_added' => 1
-                            ]);
-                        }catch(\Exception $e){
-                            Log::error('Oops! Something Went Wrong. Registration Failed', $e->getMessage());
-                        }
-                        
+                    if($update){
                         return $this->success('Great! Basic Information Added Successfully.', null, null, 201);
                     }else{
                         return $this->error('Oops! Something Went Wrong. Registration Failed.', null, null, 500);
                     }
+                }else{
+                    $check_if_phone_exists = CaregiverProfileRegistration::where('phone', $request->phone)->exists();
+                    $check_if_ssn_exists = CaregiverProfileRegistration::where('ssn', $request->ssn)->exists();
+                    if($check_if_phone_exists){
+                        return $this->error('Oops! Failed To Register. Phone Number Associated With Another User.', null, null, 400);
+                    }else if($check_if_ssn_exists){
+                        return $this->error('Oops! Failed To Register. SSN Number Associated With Another User.', null, null, 400);
+                    }else{
+    
+                        if ($request->hasFile('photo')) {
+                            $image = time() . '.' . $request->photo->extension();
+                            $request->photo->move(public_path('Caregiver/Uploads/Profile/image/'), $image);
+                            $imageName = 'Caregiver/Uploads/Profile/image/' . $image;
+                        }
+                       
+                        $create = CaregiverProfileRegistration::create([
+                            'user_id' => Auth::user()->id,
+                            'photo' => $imageName,
+                            'phone' => $request->phone,
+                            'dob' =>  Carbon::createFromFormat('m-d-Y',$request->dob)->format('Y-m-d'),
+                            'gender' => $request->gender,
+                            'ssn' => $request->ssn,
+                            'full_address' => $request->full_address,
+                            'short_address' => $request->short_address
+                        ]);
+    
+                        if($create){
+                            try{
+                                CaregiverStatusInformation::create([
+                                    'user_id' => Auth::user()->id,
+                                    'is_basic_info_added' => 1
+                                ]);
+                            }catch(\Exception $e){
+                                Log::error('Oops! Something Went Wrong. Registration Failed', $e->getMessage());
+                            }
+                            
+                            return $this->success('Great! Basic Information Added Successfully.', null, null, 201);
+                        }else{
+                            return $this->error('Oops! Something Went Wrong. Registration Failed.', null, null, 500);
+                        }
+                    }
                 }
+                
             }catch(\Exception $e){
                 return $this->error('Oops! Something Went Wrong.'.$e->getMessage(), null, null, 500);
             }
