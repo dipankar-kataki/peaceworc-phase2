135344db7c6fca0a39cf8c41f650ffb8280d5dd9
dipankar kataki <dipankar.kataki@ekodus.com>
2023-04-04 14:18:43 +0530

    agency post job

--- app/Http/Controllers/Agency/Job/PostJobController.php
+++ app/Http/Controllers/Agency/Job/PostJobController.php
@@ -128,15 +128,7 @@ class PostJobController extends Controller
             if($_GET['id'] == 0){
                 $job_details = AgencyPostJob::where('user_id', Auth::user()->id)
                             ->where('payment_status', 1)
-                            ->orWhere('status', JobStatus::Open)
-                            ->orWhere('status', JobStatus::BiddingStarted)
-                            ->orWhere('status', JobStatus::BiddingEnded)
-                            ->orWhere('status', JobStatus::QuickCall)
-                            
-                            // ->where('status', JobStatus::Open)
-                            // ->orWhere('status', JobStatus::BiddingStarted)
-                            // ->orWhere('status', JobStatus::BiddingEnded)
-                            // ->orWhere('status', JobStatus::QuickCall)
+                            ->whereIn('status',[ JobStatus::Open,JobStatus::BiddingStarted,JobStatus::BiddingEnded,JobStatus::QuickCall])
                             ->latest()->paginate('5');
                 return $this->success('Great! Job Fetched Successfully', $job_details, null, 200);
             }else{
