046d5be0a0cb8983a18010f6057004662f674875
dipankar kataki <dipankar.kataki@ekodus.com>
2023-04-07 16:03:20 +0530

    get profile agency

--- app/Http/Controllers/Agency/Auth/SignUpController.php
+++ app/Http/Controllers/Agency/Auth/SignUpController.php
@@ -152,6 +152,8 @@ class SignUpController extends Controller
     
                         $time_diff_in_minutes = $current_time->diffInMinutes($otp_validity_time);
 
+                        return response($time_diff_in_minutes);
+
                         if( $time_diff_in_minutes > 3){
                             return $this->error('Oops! OTP Expired.', null, null, 400);
                         }else{
