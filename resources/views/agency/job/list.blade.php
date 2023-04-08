2886c2ee421b2ebc95c66fa7da3792602c6e79b1
dipankar kataki <dipankar.kataki@ekodus.com>
2023-04-08 11:01:40 +0530

    admin part jobs

--- resources/views/agency/job/list.blade.php
+++ resources/views/agency/job/list.blade.php
@@ -44,14 +44,30 @@
                                     <td>{{$jobs->amount}}</td>
                                     <td>
                                         @if($jobs->payment_status === 1)
-                                            <a href="javascript:void(0);" class="fs-14 badge badge-soft-success">SUCCESSFUL</a>
+                                            <a href="javascript:void(0);" class="fs-14 badge text-bg-success">SUCCESSFUL</a>
                                         @elseif($jobs->payment_status === 0)
-                                            <a href="javascript:void(0);" class="fs-14 badge badge-soft-warning">PENDING</a>
+                                            <a href="javascript:void(0);" class="fs-14 badge text-bg-warning">PENDING</a>
                                         @else
-                                            <a href="javascript:void(0);" class="fs-14 badge badge-soft-danger">FAILED</a>
+                                            <a href="javascript:void(0);" class="fs-14 badge text-bg-danger">FAILED</a>
                                         @endif
                                     </td>
-                                    <td><span style="text-transform:uppercase;">{{$jobs->status}}</span></td>
+                                    <td>
+                                        @if ($jobs->status === 'Open Job')
+                                            <span class="badge badge-label bg-primary fs-12"><i class="mdi mdi-circle-medium"></i>{{$jobs->status}}</span>
+                                        @elseif($jobs->status === 'Ongoing Job')
+                                            <span class="badge badge-label bg-secondary fs-12"><i class="mdi mdi-circle-medium"></i> {{$jobs->status}}</span>
+                                        @elseif($jobs->status === 'Completed Job' || $jobs->status === 'Closed' )
+                                            <span class="badge badge-label bg-success fs-12"><i class="mdi mdi-circle-medium"></i> {{$jobs->status}}</span>
+                                        @elseif($jobs->status === 'Deleted' || $jobs->status === 'Quick Call' )
+                                            <span class="badge badge-label bg-danger fs-12"><i class="mdi mdi-circle-medium"></i> {{$jobs->status}}</span>
+                                        @elseif($jobs->status === 'Bidding Started' || $jobs->status === 'Bidding Ended' || $jobs->status === 'Upcoming')
+                                            <span class="badge badge-label bg-info fs-12"><i class="mdi mdi-circle-medium"></i> {{$jobs->status}}</span>
+                                        @elseif($jobs->status === 'Pending' || $jobs->status === 'On Hold' )
+                                            <span class="badge badge-label bg-warning fs-12"><i class="mdi mdi-circle-medium"></i> {{$jobs->status}}</span>
+                                        @elseif($jobs->status === 'Expired' ||  $jobs->status === 'Cancelled' )
+                                            <span class="badge badge-label bg-dark fs-12"><i class="mdi mdi-circle-medium"></i> {{$jobs->status}}</span>
+                                        @endif
+                                    </td>
                                     <td>
                                         <div class="dropdown d-inline-block">
                                             <button class="btn btn-soft-secondary btn-sm dropdown" type="button" data-bs-toggle="dropdown" aria-expanded="false">
