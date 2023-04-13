5a6ac06986ec2cc9f5fafea61ac0a22142ee33d4
dipankar kataki <dipankar.kataki@ekodus.com>
2023-04-13 11:51:31 +0530

    admin dashboard greeting message updated

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -8,7 +8,40 @@
                     <div class="col-12">
                         <div class="d-flex align-items-lg-center flex-lg-row flex-column">
                             <div class="flex-grow-1">
-                                <h4 class="fs-16 mb-1">Good Morning, Admin!</h4>
+                                <h4 class="fs-16 mb-1">
+                                    <?php 
+                                        
+                                        $greetings = "";
+
+                                        /* This sets the $time variable to the current hour in the 24 hour clock format */
+                                        $time = date("H");
+
+                                        /* Set the $timezone variable to become the current timezone */
+                                        $timezone = date("e");
+
+                                        /* If the time is less than 1200 hours, show good morning */
+                                        if ($time < "12") {
+                                            $greetings = "Good Morning";
+                                        } else
+
+                                        /* If the time is grater than or equal to 1200 hours, but less than 1700 hours, so good afternoon */
+                                        if ($time >= "12" && $time < "17") {
+                                            $greetings = "Good Afternoon";
+                                        } else
+
+                                        /* Should the time be between or equal to 1700 and 1900 hours, show good evening */
+                                        if ($time >= "17" && $time < "19") {
+                                            $greetings = "Good Evening";
+                                        } else
+
+                                        /* Finally, show good night if the time is greater than or equal to 1900 hours */
+                                        if ($time >= "19" && $time <= "24") {
+                                            $greetings = "Good Night";
+                                        }    
+                                        
+                                    ?>
+                                    
+                                    {{$greetings }}, Admin!</h4>
                                 <p class="text-muted mb-0">Here's what's happening with Peaceworc
                                     today.</p>
                             </div>
