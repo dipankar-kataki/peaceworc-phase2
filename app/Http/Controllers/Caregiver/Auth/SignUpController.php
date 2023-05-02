7d4cd6de86349785dbaf9328f03515c4640409d4
dipankar kataki <dipankar.kataki@ekodus.com>
2023-05-02 18:22:07 +0530

    verify otp

--- app/Http/Controllers/Caregiver/Auth/SignUpController.php
+++ app/Http/Controllers/Caregiver/Auth/SignUpController.php
@@ -147,7 +147,7 @@ class SignUpController extends Controller
                         // $time_diff_in_minutes = $current_time->diffInMinutes($otp_val);
                         $time_diff_in_minutes =  $current_time->diff(new DateTime($otp_validity_time));
 
-                        return response()->json(['Difference' =>  $time_diff_in_minutes]);
+                        return response()->json(['Difference' =>  $time_diff_in_minutes->i]);
 
                         if( $time_diff_in_minutes >= 3){
                             return $this->error('Oops! OTP Expired.', null, null, 400);
