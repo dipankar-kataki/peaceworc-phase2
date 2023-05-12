ba088e619da99378bfde29d3774c12ddcbf515b6
dipankar kataki <dipankar.kataki@ekodus.com>
2023-05-12 12:43:46 +0530

    admin login response changed

--- app/Http/Controllers/Admin/Auth/LoginController.php
+++ app/Http/Controllers/Admin/Auth/LoginController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Admin\Auth;
 
 use App\Common\Role;
 use App\Http\Controllers\Controller;
+use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Validator;
@@ -23,8 +24,10 @@ class LoginController extends Controller
                 if (!Auth::attempt(['email' => $request->email, 'password' => $request->password, 'role' => Role::Admin])) {
                     return response()->json(['message' => 'Login Failed. Credentials did not match with our records.', 'status' => 0]);
                 }else{
+                    $user = User::where('email', $request->email)->firstOrFail();
+                    $auth_token =  $user->createToken('auth_token')->plainTextToken;
                     // return response()->json(['message' => 'Login Successful', 'url' => "/admin/dashboard",'status' => 1]);
-                    return response()->json(['message' => 'Login Successful', 'status' => 1]);
+                    return response()->json(['message' => 'Login Successful', 'access_token' => $auth_token, 'status' => 1]);
                 }
             }catch(\Exception $e){
                 return response()->json(['message' => 'Something Went Wrong While Login.', 'status' => 0]);
