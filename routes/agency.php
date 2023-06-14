92c9560ca2ee397a223bde644e2dbe5af174583a
dipankar kataki <dipankar.kataki@ekodus.com>
2023-06-14 10:16:52 +0530

    removing comments

--- routes/agency.php
+++ routes/agency.php
@@ -45,10 +45,6 @@ use Illuminate\Support\Facades\Route;
     
 // });
 
-
-// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-//     return $request->user();
-// });
 Route::group(['middleware' => ['auth:sanctum']], function () {
 
     Route::group(['prefix' => 'owner'], function(){
