f08cc6513ca8cbedea1aed2023a0a5b61aa0d030
dipankar kataki <dipankar.kataki@ekodusinc.com>
2023-08-21 17:35:06 +0530

    peaceworc landing enquiry table modified

--- routes/web.php
+++ routes/web.php
@@ -19,7 +19,6 @@ Route::get('/', function () {
 })->name('web.landing');
 
 Route::get('', [SiteController::class, 'index'])->name('site.index');
-// Route::get('blog/{id?}/{viewAs?}', [SiteController::class, 'blogs'])->name('site.blog');
 Route::post('contact', [SiteController::class, 'contact'])->name('site.contact');
 Route::get('terms-and-conditions', [SiteController::class, 'terms'])->name('site.terms');
 Route::get('privacy-policy', [SiteController::class, 'privacy'])->name('site.privacy');
