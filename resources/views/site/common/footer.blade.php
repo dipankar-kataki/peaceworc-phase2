f0ec05275736841026619e5b23b4e0722732bc16
dipankar kataki <dipankar.kataki@ekodusinc.com>
2023-08-22 17:40:19 +0530

    landing page

--- resources/views/site/common/footer.blade.php
+++ resources/views/site/common/footer.blade.php
@@ -1,7 +1,7 @@
 <footer class="footer mt-auto py-3 bg-light">
     <div class="container">
         <div class="d-md-flex justify-content-between align-items-center">
-            <p class="text-muted mb-0 py-md-3 text-md-start text-center"> &copy; 2022 | PeaceWorc | Designed & Developed By <a
+            <p class="text-muted mb-0 py-md-3 text-md-start text-center"> &copy; <?php echo date('Y'); ?> | PeaceWorc | Designed & Developed By <a
                     href="https://ekodus.com/" target="_blank" class="link text-muted">Ekodus Technologies</a></p>
             <p class="text-muted mb-0 py-md-3 text-md-start text-center"> <a class="text-muted link" target="_blank" href="{{ route('site.privacy') }}">Privacy
                     Policy</a> | <a class="text-muted link" target="_blank" href="{{ route('site.terms') }}">Terms & Conditions</a> 
