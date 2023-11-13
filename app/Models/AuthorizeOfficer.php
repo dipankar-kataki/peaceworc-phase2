3e65c06c830abe143bbff905d710eb224f35367d
dipankar kataki <dipankar.kataki@ekodusinc.com>
2023-11-13 12:00:09 +0530

    delete auth officer fixed

--- app/Models/AuthorizeOfficer.php
+++ app/Models/AuthorizeOfficer.php
@@ -4,11 +4,10 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\SoftDeletes;
 
 class AuthorizeOfficer extends Model
 {
-    use HasFactory, SoftDeletes;
+    use HasFactory;
 
     protected $table = 'authorize_officers';
     protected $guarded = [];
