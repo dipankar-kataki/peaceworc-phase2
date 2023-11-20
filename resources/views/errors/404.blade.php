1c102474ef63bd38bcd1ccde09c0441edcc5b79f
dipankar kataki <dipankar.kataki@ekodusinc.com>
2023-11-20 11:47:01 +0530

    404 custom page created

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta content="Peaceworc a caregiving organisation" name="description" />
+    <meta content="Peaceworc" name="author" />
+    <!-- App favicon -->
+    <link rel="shortcut icon" href="/assets/images/logo/logo.png">
+    <title>Page not found</title>
+
+    <!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css">
+
+    <style>
+        .page-not-found-content{
+            height:100vh;
+        }
+        h1,p{
+            color:midnightblue;
+        }
+    </style>
+</head>
+<body>
+
+    <div class="container">
+        <div class="page-not-found-content d-flex flex-column justify-content-center align-items-center">
+            <div class="logo mb-4">
+                <img src="/assets/images/logo/logo.png" alt="Peaceworc Logo" style="width:120px;">
+            </div>
+            <h1>404 - Page Not Found</h1>
+            <p>Sorry, the page you are looking for might be in another castle!</p>
+        </div>
+    </div>
+    {{-- <!-- jQuery library -->
+    <script src="https://cdn.jsdelivr.net/npm/jquery@3.7.1/dist/jquery.slim.min.js"></script>
+
+    <!-- Popper JS -->
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
+
+    <!-- Latest compiled JavaScript -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js"></script> --}}
+</body>
+</html>
